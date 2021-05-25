92086af887dc99ec0c4a858a3ab7f7d32af9c828
goutham-o <studygouthamm@gmail.com>
2021-05-25 22:26:34 +0530

    read components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import data from './data'
+import Product from './components/Product'
 
-function App() {
+export const App = () => {
   return (
     <div className='grid-container'>
       <header className='row'>
@@ -18,34 +19,7 @@ function App() {
       <main>
         <div className='row center'>
           {data.products.map((product) => (
-            <div className='card' key={product._id}>
-              <a href={`/product/${product._id}`}>
-                <img className='medium' src={product.image} alt={product.name} />
-              </a>
-              <div className='card-body'>
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className='rating'>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star-half-o'></i>
-                  </span>
-                </div>
-                <div className='price'>${product.price}</div>
-              </div>
-            </div>
+            <Product key={product._id} product={product} />
           ))}
         </div>
       </main>
